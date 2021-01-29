2ee43de9a6d72a1a3009e1fa58e252131dc2624a
Mehmet Baysal <mbaysal212@outlook.com>
2021-01-30 01:37:52 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer3)
+        {
+            Console.WriteLine("\n***Müşteri Kaydı Başarılı***");
+            Console.WriteLine("Müşteri Ad: " + customer3.FirstName + "Deniz");
+            Console.WriteLine("Müşteri Soyad: " + customer3.LastName + "Gök");
+            Console.WriteLine("Müşteri Hesap Türü: " + customer3.AccountType + 1);
+        }
+
+        public void Update(Customer customer4)
+        {
+            Console.WriteLine("\n***Müşteri Bilgileri Güncellendi***");
+            Console.WriteLine("Müşteri Ad: " + customer4.FirstName + "Seda");
+            Console.WriteLine("Müşteri Soyad: " + customer4.LastName + "Şeker");
+            Console.WriteLine("Müşteri Hesap Türü: "+ customer4.AccountType + 2);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t-----Yönetici Banka Kayıt Forum Kontrolü------");
+            Customer customer = new Customer();
+            customer.Id = 10;
+            customer.FirstName = "Ali";
+            customer.LastName = "Genç";
+            customer.IdCard = "11111111111";
+            customer.AccountType = 2;
+
+            Customer customer1 = new Customer();
+            customer1.Id = 11;
+            customer1.FirstName = "Seda";
+            customer1.LastName = "Şeker";
+            customer1.IdCard = "22222222222";
+            customer1.AccountType = 1;
+
+
+
+            Customer[] customers = new Customer[] {customer,customer1 };
+            foreach (var Customer in customers)
+            {
+                Console.WriteLine("\nMüşteri Ad: " + Customer.FirstName );
+                Console.WriteLine("Müşteri Soyad: " + Customer.LastName);
+                Console.WriteLine("Müşteri Hesap Türü: " + Customer.AccountType);
+            }
+            
+            Customer customer3 = new Customer ();
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer3);
+
+            Customer customer4 = new Customer();
+            CustomerManager customerManager1 = new CustomerManager();
+            customerManager.Update(customer4);
+        }
+
+    }
+}
